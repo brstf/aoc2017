f91cd8f5a4f08bdeaf722833860217a9d7294746
brstf <brstf.dev@gmail.com>
2017-12-03 18:52:38 -0500

    Completed day 2

--- src/aoc2017/core.clj
+++ src/aoc2017/core.clj
@@ -24,3 +24,26 @@
        (partition 2)
        (take (count captcha))
        (transduce captcha-xf +)))
+
+(defn str->table [s]
+  (->> (clojure.string/split s #"\n")
+       (map #(map  str->int (clojure.string/split % #"\s")))))
+
+(defn row-range
+  [row]
+  (- (apply max row) (apply min row)))
+
+(defn evenly-div
+  [row]
+  (let [indexed-row (zipmap (range) row)]
+    (first (for [[i1 n1] indexed-row [i2 n2] indexed-row
+                 :when (and (not= i1 i2) (= 0 (rem n1 n2)))]
+             (/ n1 n2)))))
+
+(defn checksum
+  [table]
+  (reduce + (map row-range table)))
+
+(defn checksum2
+  [table]
+  (reduce + (map evenly-div table)))
